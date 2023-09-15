bf111c6c8a26f82378e98861f78de020158a8045
priyanshu1831 <kaushik.priyanshu22@gmail.com>
2023-09-15 22:54:21 +0530

    last

--- Frontend/src/app/piechart/piechart.component.ts
+++ Frontend/src/app/piechart/piechart.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js'
 
 @Component({
   selector: 'app-piechart',
   templateUrl: './piechart.component.html',
   styleUrls: ['./piechart.component.css']
 })
-export class PiechartComponent {
+export class PiechartComponent implements OnInit {
+  public chart: any;
 
+ngOnInit(): void {
+    this.createChart();
+  }
+
+createChart() {
+    Chart.register(
+      ...registerables);
+
+this.chart = new Chart("myChart", {
+  type: 'pie',
+  data: {
+    labels: ['Red', 'Pink', 'Green', 'Yellow', 'Orange', 'Blue'],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 240, 100, 432, 253, 34],
+      backgroundColor: [
+        'red',
+        'pink',
+        'green',
+        'yellow',
+        'orange',
+        'blue',
+      ],
+      hoverOffset: 4
+    }],
+  },
+  options: {
+    aspectRatio: 2.5
+  }
+});
+
+}
 }
